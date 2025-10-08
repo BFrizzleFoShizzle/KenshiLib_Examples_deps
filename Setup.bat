22d66a165ee51a85ef6e85c400a9063ecb93cfcd
Bryden Frizzell <bryden.frizzell@gmail.com>
2025-10-08 16:37:32 +1300

    Updated boost setup

--- Setup.bat
+++ Setup.bat
@@ -4,8 +4,11 @@ IF NOT EXIST boost_1_60_0/boost.zip (
 	echo Please run "git lfs pull" to download
 	exit
 )
-IF NOT EXIST boost_1_60_0/boost (
-	echo Extracting boost
-	tar -xf boost_1_60_0/boost.zip --directory boost_1_60_0/
-)
+IF NOT EXIST boost_1_60_0/boost GOTO extract
+IF NOT EXIST boost_1_60_0/stage GOTO extract
+GOTO extracted
+:extract
+echo Extracting boost
+tar -xf boost_1_60_0/boost.zip --directory boost_1_60_0/
+:extracted
 powershell -NoProfile -ExecutionPolicy Bypass Scripts/setup_environment_vars.ps1
