8996a62c17bf6bb9f61fcfe373dba3507c6113c3
Bryden Frizzell <bryden.frizzell@gmail.com>
2025-10-08 16:10:27 +1300

    Setup script tweak

--- Setup.bat
+++ Setup.bat
@@ -8,4 +8,4 @@ IF NOT EXIST boost_1_60_0/boost (
 	echo Extracting boost
 	tar -xf boost_1_60_0/boost.zip --directory boost_1_60_0/
 )
-powershell Scripts/setup_environment_vars.ps1
+powershell -NoProfile -ExecutionPolicy Bypass Scripts/setup_environment_vars.ps1
